ba92433ad088e6a6000113a3a24693b242c4298b
sondoquang <sondoquang3@gmail.com>
2024-08-27 22:51:47 +0700

    them-moi

--- src/myProject/main.java
+++ src/myProject/main.java
@@ -6,5 +6,8 @@ public class main {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("expected: 5! = 120; actual: "+ result);
+        System.out.println("expected: 5! = 120; actual: "+ result);
+        System.out.println("expected: 5! = 120; actual: "+ result);
+        
     }
 }
